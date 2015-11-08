ccfd21c653079cddfa839099a683e688f0a07369
Michael Huang <myh1000@gmail.com>
2015-11-07 17:25:35 -0800

    more changes

--- playerID.js
+++ playerID.js
@@ -3,30 +3,32 @@ $(document).ready(function(){
   $('#submitlookup').on('click', function(event){
     event.preventDefault();
     ID = $("#summID").val();
+  if (ID !== "") {
+      $.ajax({
+          url: 'rosterPull.cs', //rosterPull here refers to the C# method
+          type: "POST",
+          data: {
+            roster: ID
+          },
+          dataType: "json",
+          contentType: "application/json; charset=utf-8",
+          success: function (resp) {
 
-    $.ajax({
-        url: "/myh1000.github.io/controllers/controller.cs", //rosterPull here refers to the C# method
-        type: "POST",
-        data: {
-          roster: ID
-        },
-        dataType: "json",
-        contentType: "application/json; charset=utf-8",
-        success: function (resp) {
+              json = JSON && JSON.parse(resp) || $.parseJSON(resp);
 
-            json = JSON && JSON.parse(resp) || $.parseJSON(resp);
+              summonerLevel = json[userID].summonerLevel;
+              summonerID = json[userID].id;
 
-            summonerLevel = json[userID].summonerLevel;
-            summonerID = json[userID].id;
-
-            document.getElementById("sLevel").innerHTML = summonerLevel;
-            document.getElementById("sID").innerHTML = summonerID;
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            if (XMLHttpRequest.status == 0) {
-              alert("error getting Summoner data!");
-            }
-        }
-    });
+              document.getElementById("sLevel").innerHTML = summonerLevel;
+              document.getElementById("sID").innerHTML = summonerID;
+          },
+          error: function (XMLHttpRequest, textStatus, errorThrown) {
+              if (XMLHttpRequest.status == 0) {
+                alert(ID);
+              }
+          }
+      });
+    }
+    else {}
   });
 });
--- rosterPull.cs
+++ rosterPull.cs
@@ -1,12 +1,12 @@
 [HttpPost]
 using System.*;
 
-public ActionResult rosterPull(string roster) {
+public static rosterPull(string roster) {
   string text = "";
   try {
     using (var webClient = new System.Net.WebClient()) {
       webClient.Encoding = Encoding.UTF8;
-      var json2 = webClient.DownloadString("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + roster + "?api_key=" + APIKEY)
+      var json2 = webClient.DownloadString("https://na.api.pvp.net/api/lol/na/v1.4/summoner/" + roster + "?api_key=7cf51bc0-7417-4b55-8d08-b9eec244bbc4")
       return Json(json2);
     }
   }
@@ -15,3 +15,4 @@ public ActionResult rosterPull(string roster) {
   }
   return Json(new { json = text });
 }
+</script>
